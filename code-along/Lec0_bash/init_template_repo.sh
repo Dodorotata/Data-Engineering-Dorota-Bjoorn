39ddcea96b8340cf0d039d392b5c852adecc950f
Dorota Bjoorn <Dorota.Bjoorn@iths2022>
2023-08-03 08:44:05 +0200

    new repo for course, code-along 00 bash finalized

---
+++ code-along/Lec0_bash/init_template_repo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "running initializations"
+sleep 1
+
+mkdir theory code-alongs  explorations
+touch theory/.gitkeep code-alongs/.gitkeep explorations/explore.txt
+
+for i in  {1..5}; do
+  echo "This is file $i" > file$i.txt
+done
+
